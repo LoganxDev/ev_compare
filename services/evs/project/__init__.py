d1e25860f240567edd8f270a3e29b1ac4f487341
Logan Lasiter <lasiterlogan@gmail.com>
2019-07-15 20:13:46 -0700

    Added flask debug toolbar.

--- services/evs/project/__init__.py
+++ services/evs/project/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -12,6 +14,7 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
+    toolbar.init_app(app)
 
     from project.api.evs import evs_blueprint
     app.register_blueprint(evs_blueprint)
